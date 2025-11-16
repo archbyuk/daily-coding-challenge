c9834dd7a41545ccd80e29c5ccdee3d73f1cec1e
JinukSynth <jinuk.work@gmail.com>
2025-11-16 19:14:33 +0900

    2025.11.16

--- js_programmers-Lv.0/Lv0-15.js
+++ js_programmers-Lv.0/Lv0-15.js
@@ -20,3 +20,24 @@ function solution(str_list) {
         return str_list.slice(0, l_first)
     }
 }
+
+// '이나'면 OR이 맞는 거 같은데, 왜 AND로 해야 통과되는지 모르겠네..
+function solution(str_list) {
+    const l_first = str_list.indexOf('l');
+    const r_first = str_list.indexOf('r');
+
+    // 둘 다 없으면 []: '이나'인데 왜 OR가 아니고 AND임? 내가 빡데가리라 이해를 못하는 건가?
+    if (l_first === -1 && r_first === -1) return [];
+
+    // r이 먼저 나옴
+    if (r_first !== -1 && (l_first === -1 || r_first < l_first)) {
+        return str_list.slice(r_first + 1);
+    }
+
+    // l이 먼저 나옴
+    if (l_first !== -1 && (r_first === -1 || l_first < r_first)) {
+        return str_list.slice(0, l_first);
+    }
+
+    return [];
+}
